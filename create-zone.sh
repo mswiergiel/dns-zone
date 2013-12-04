5f2330155c71e5c678880166a4e599c765fe789b
mswiergiel <mikolaj.swiergiel@gmail.com>
2013-12-04 20:41:18 +0100

    create-zone.sh

--- create-zone.sh
+++ create-zone.sh
@@ -9,15 +9,15 @@ source "$sourcefile"
 
 
 echo "\$ORIGIN ."
-echo "$domain       IN SOA $nameserver. hostmaster.int.example.com. ("
+echo "$domain       IN SOA ${nameserver}. hostmaster.int.example.com. ("
 echo "              $serial     ;serial"
 echo "              $refresh    ;refresh (1 day)"
 echo "              $retry      ;retry"
 echo "              $expire     ;expire"
 echo "              $minimum    ;minimum"
 echo "              )"
-echo "              NS  $domain."
-echo "\$ORIGIN $domain."
+echo "              NS  ${domain}."
+echo "\$ORIGIN ${domain}."
 echo "\$INCLUDE $static"
 echo ""
 
